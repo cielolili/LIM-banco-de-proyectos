e84038f8d84753ec606579b32329bda9bb94a10d
cielolili <cielito.torres@hotmail.com>
2020-05-21 20:25:22 -0500

    test de función MoreImages

---
+++ src/test/MoreImages.spec.jsx
@@ -0,0 +1,22 @@
+import MoreImages from '../controllerAPI/MoreImages';
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+describe('MoreImages', () => {
+  it('Debería traerme 20 imágenes adicionales al hacer scrroll', async () => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      const data = new Promise((resolve) => {
+        resolve({
+          json() {
+            return { page: 2, query: 'Mickey' };
+          },
+        });
+      });
+      return data;
+    });
+    const response = await MoreImages();
+    expect(response.page).toBe(2);
+    expect(response.query).toBe('Mickey');
+  });
+});
