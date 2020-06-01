6368cc616857c90f64aa07093b82a3bafea0b964
cielolili <cielito.torres@hotmail.com>
2020-06-01 18:58:48 -0500

    Updates

---
+++ precache-manifest.cf5de8d43a4bb1480b104f3790bf9698.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e671b72dd65fda93c4375bc5b22ea9",
+    "url": "/LIM-banco-de-proyectos/index.html"
+  },
+  {
+    "revision": "1cbc22d402bf8bc61cff",
+    "url": "/LIM-banco-de-proyectos/static/css/main.675c91d1.chunk.css"
+  },
+  {
+    "revision": "b344f41b286ca4a62920",
+    "url": "/LIM-banco-de-proyectos/static/js/2.edc6b834.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/LIM-banco-de-proyectos/static/js/2.edc6b834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbc22d402bf8bc61cff",
+    "url": "/LIM-banco-de-proyectos/static/js/main.108df21c.chunk.js"
+  },
+  {
+    "revision": "919f1646fb8077801ae4",
+    "url": "/LIM-banco-de-proyectos/static/js/runtime-main.7a83856d.js"
+  },
+  {
+    "revision": "71152490836efb997681f2463655bf53",
+    "url": "/LIM-banco-de-proyectos/static/media/like.71152490.svg"
+  },
+  {
+    "revision": "4bb59c70011ea4d222b80bffcacea0d0",
+    "url": "/LIM-banco-de-proyectos/static/media/lupa.4bb59c70.png"
+  },
+  {
+    "revision": "e73e4fbd976a1fa227ddbf328f7a0ff4",
+    "url": "/LIM-banco-de-proyectos/static/media/pinterest.e73e4fbd.svg"
+  }
+]);
